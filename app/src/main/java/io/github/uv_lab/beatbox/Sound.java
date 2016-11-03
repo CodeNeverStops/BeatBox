ffe5ca06c98e2385f80c2b54d22875e4b375973a
Wayne.You <lokiwuxi@gmail.com>
2016-11-03 13:21:01 +0800

    load sounds

---
+++ app/src/main/java/io/github/uv_lab/beatbox/Sound.java
@@ -0,0 +1,25 @@
+package io.github.uv_lab.beatbox;
+
+/**
+ * Created by youwei on 2016/11/3.
+ */
+
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
